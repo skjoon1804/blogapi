e5ab2c7d64d9f47e1c062c034560354aaba6c876
skjoon1804 <skjoon1804@naver.com>
2019-06-22 04:52:26 -0700

    Update router

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -40,7 +40,31 @@ router.delete("/:id", (req, res) => {
 
 // UPDATE
 router.put("/:id", jsonParser, (req, res) => {
-  const requiredFields = [];
+  const requiredFields = ["title", "content", "author", "publishDate", "id"];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+    if (req.params.id !== req.body.id) {
+      const message = `Request path id ${req.params.id} and request body id ${
+        req.body.id
+      } must match`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+    console.log(`Updating shopping list item ${req.params.id}`);
+    const updatedItem = BlogPosts.update({
+      id: req.params.id,
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author,
+      publishDate: req.body.publishDate
+    });
+    res.status(204).end();
+  }
 });
 
 module.exports = router;
