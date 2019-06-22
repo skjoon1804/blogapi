b9163b73ed390b249085642dad0e60b16faa70f5
skjoon1804 <skjoon1804@naver.com>
2019-06-22 04:07:53 -0700

    Call create method in model.js

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -21,6 +21,13 @@ router.post("/", jsonParser, (req, res) => {
       console.error(message);
       return res.status(400).send(message);
     }
+    const item = BlogPosts.create(
+      req.body.title,
+      req.body.content,
+      req.body.author,
+      req.body.publishDate
+    );
+    res.status(201).json(item);
   }
 });
 
